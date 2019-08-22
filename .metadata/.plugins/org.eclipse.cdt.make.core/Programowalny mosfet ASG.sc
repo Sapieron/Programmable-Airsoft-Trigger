15bb8ddc3c90efe1fac561e4f712fcfc0b7b7537
Sapieron <pawelochojec@gmail.com>
2019-08-22 07:21:44 +0200

    initial commit

---
+++ .metadata/.plugins/org.eclipse.cdt.make.core/Programowalny mosfet ASG.sc
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<?scdStore version="2"?><scannerInfo id="org.eclipse.cdt.make.core.discoveredScannerInfo">
+<instance id="fr.ac6.managedbuild.config.gnu.cross.exe.debug.1362186347;fr.ac6.managedbuild.config.gnu.cross.exe.debug.1362186347.;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.2020154712;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.input.c.1576827662">
+<collector id="org.eclipse.cdt.make.core.PerProjectSICollector"/>
+</instance>
+<instance id="fr.ac6.managedbuild.config.gnu.cross.exe.release.616792000;fr.ac6.managedbuild.config.gnu.cross.exe.release.616792000.;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.646747457;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.input.c.299614435">
+<collector id="org.eclipse.cdt.make.core.PerProjectSICollector"/>
+</instance>
+</scannerInfo>
---
+++ Programowalny mosfet ASG/Debug/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c 
+
+OBJS += \
+./src/main.o 
+
+C_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F0 -DSTM32F030C8Tx -DDEBUG -I"C:/Users/Pawel/Documents/ST/STM32F030 - mosfet progr/Programowalny mosfet ASG/drivers/inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
